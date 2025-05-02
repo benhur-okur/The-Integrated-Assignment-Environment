dea1da6a55c20052ed7830fa6086145efbb59994
Efe Serin <efe.serin999@gmail.com>
2025-05-03 01:03:05 +0300

    back button problem fixed
    there was an issue with the back button on assignment report window

--- The-Integrated-Assignment-Environment/AssignmentReportWindow.xaml.cs
+++ The-Integrated-Assignment-Environment/AssignmentReportWindow.xaml.cs
@@ -1,28 +1,50 @@
+using System.Collections.ObjectModel;
 using System.Windows;
+using The_Integrated_Assignment_Environment.Models;
+using The_Integrated_Assignment_Environment.Services;
 
 namespace The_Integrated_Assignment_Environment;
 
 public partial class AssignmentReportWindow : Window
 {
+    private ObservableCollection<Configuration> configurations;
     public AssignmentReportWindow()
     {
         InitializeComponent();
-
     }
+    
+    private void LoadConfigurations()
+    {
+        configurations = ConfigurationService.LoadAll();
+        //cmbConfiguration.ItemsSource = configurations;
+        //cmbConfiguration.DisplayMemberPath = "LanguageName";
+    }
+    
 
     private void btnSelectZipDirectory_Click(object sender, RoutedEventArgs e)
     {
-        // TODO: ZIP klasör seçimi yapılacak (şu anda boş)
+        // TODO: ZIP klasör seçimi yapılacak
     }
 
     private void btnProcessAssignments_Click(object sender, RoutedEventArgs e)
     {
-        // TODO: ZIP dosyalarını işleyip sonuçları gösterecek (şu anda boş)
+        // TODO: ZIP dosyalarını işleyip sonuçları gösterecek
     }
 
+    private void btnBack_Click(object sender, RoutedEventArgs e)
+    {
+        var createAssignmentWindow = new CreateAssignmentWindow();
+        createAssignmentWindow.Show();
+        this.Close();
+    }
+    private void btnNewConfiguration_Click(object sender, RoutedEventArgs e)
+    {
+        var configWindow = new ConfigurationWindow();
+        configWindow.ShowDialog();
 
-
-
+        // Config penceresi kapandıktan sonra yeniden yükle
+        LoadConfigurations();
+    }
     public class AssignmentResult
     {
         public string StudentId { get; set; }
