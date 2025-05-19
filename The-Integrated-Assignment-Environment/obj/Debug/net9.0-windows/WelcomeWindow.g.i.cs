dc1ae97295a71e115380fc6b145185b4b2aa1034
Ilaydabuzbuz <119805453+Ilaydabuzbuz@users.noreply.github.com>
2025-05-19 15:00:56 +0300

    Installer

--- The-Integrated-Assignment-Environment/obj/Debug/net9.0-windows/WelcomeWindow.g.i.cs
+++ The-Integrated-Assignment-Environment/obj/Debug/net9.0-windows/WelcomeWindow.g.i.cs
@@ -47,13 +47,13 @@ namespace The_Integrated_Assignment_Environment {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.5.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/The-Integrated-Assignment-Environment;V1.0.0.0;component/welcomewindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/The-Integrated-Assignment-Environment;component/welcomewindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\WelcomeWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -63,7 +63,7 @@ namespace The_Integrated_Assignment_Environment {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.5.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
